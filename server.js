78784b3bb1836d2209d64510e3ee2fa37a7664dc
Antonis <antonistodoris@gmail.com>
2019-01-20 14:13:21 +0100

    Created APIs for sign In Register and image submitting, also created a get API for all users and by user ID

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'Antonis',
+			email: 'antonistodoris@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Vasiliki',
+			email: 'vasilikitodori@hotmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.listen(3000, ()=>{
+	console.log('app is running on port 3000');
+})
+
+
+app.get('/', (req, res) => {
+	res.json(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+		res.json('Success');
+	} else{
+		res.status(400).json('error logging in');
+	}
+})
+
+app.post('/register', (req, res) => {
+	const { name, password, email } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res)=> {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user=> {
+		if(user.id === id){
+			found = true;
+			return res.json(user);
+		}
+	})
+	if(!found){
+		res.status(400).json('not found');
+	}
+})
+
+
+app.put('/image', (req,res)=>{
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user=> {
+		if(user.id === id){
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	})
+	if(!found){
+		res.status(400).json('not found');
+	}
+})
+
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
