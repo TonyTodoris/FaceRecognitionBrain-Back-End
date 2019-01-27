aba81fd81270883b5eb61383aff9d6f6cc61bbb5
Antonis <antonistodoris@gmail.com>
2019-01-27 17:53:14 +0100

    Added enviromental variablr for deployment

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.listen(Port, ()=>{
 	console.log(`app is running on port ${Port}`);
 })
 */
-app.listen(3000, ()=>{
-  console.log(' app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log( `app is running on port${process.env.PORT}` );
 })
 
 app.get('/', (req, res) => {
